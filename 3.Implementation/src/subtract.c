aa6c603b059bf3140cd6bef803fdcf8d09434ed5
99003572 <78578826+99003572@users.noreply.github.com>
2021-02-05 22:19:34 +0530

    Update subtract.c

--- 3.Implementation/src/subtract.c
+++ 3.Implementation/src/subtract.c
@@ -1,10 +1,8 @@
 #include "header.h"
-void subtract_numbers()
+
+ int subtract(int num1,int num2)
 {
-    int num1, num2;
-    printf(" Enter the first number\n");
-    scanf("%d", &num1);
-    printf(" Enter the second number\n");
-    scanf("%d", &num2);
-    printf("%d - %d = %d\n",num1,num2,num1-num2);
+	int subtract=0;
+	subtract = num1-num2;
+	return subtract;
 }
