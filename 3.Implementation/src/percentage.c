817f9438d1d96a5ba7bc5c4fc905784c6ef1dad4
99003569 <78537068+99003569@users.noreply.github.com>
2021-02-07 12:51:22 +0530

    Update percentage.c

--- 3.Implementation/src/percentage.c
+++ 3.Implementation/src/percentage.c
@@ -4,15 +4,14 @@
 float percentage(float num1,float num2)
 {
     float per;
-    /*printf("Enter 2 numbers: \n");
+   
     scanf("%f %f", &num1,&num2);*/
     if(num1==0 || num2==0)
     {
         printf("Both the numbers entered should be greater than 0");
     }
     else
-    {/* Calculate percentage */
-    /*Formula for percentage is */
+    {
        
     per = (num1 / num2 )*100;
     
