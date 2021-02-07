fe6dc01a0a532c42f7fc49b38cf51b123c426549
99003569 <78537068+99003569@users.noreply.github.com>
2021-02-07 19:39:43 +0530

    Update main.c

--- 3.Implementation/main.c
+++ 3.Implementation/main.c
@@ -1,14 +1,14 @@
+  
 #include "header.h"
+#include"test.h"
 #include <stdio.h>
 
 int main()
 {
     int select,number1,number2;
-    float num1,num2;
     printf("press 1 to add two numbers\n press 2 to subtract two numbers\n press 3 to multiply two numbers\n");
     printf("press 4 to divide two numbers\n press 5 to find modulus\n");
     printf("press 6 to find exponential\n press 7 to find factorial of a numbers\n");
-    printf("press 8 to find percentage\n press 9 to find square of a number \n");
     scanf("%d", &select);
     switch(select)
     {
@@ -71,24 +71,10 @@ int main()
 	        printf("Result is %d",factorial(number1));
 	        break;
                    }
-        case 8: {
-            printf("Enter the obtained number: ");
-	        scanf("%f",&num1);
-	        printf("Enter the total\n");
-	        scanf("%f",&num2);
-	        printf("Result is %f",percentage(num1,num2));
-	        break;
-                   }
-	case 9: {
-            printf("Enter the number: ");
-	        scanf("%f",&num1);
-	        printf("Result is %f",square(num1));
-	        break;
-                   }
-		    
-    case 10: printf("Invalid choice");
+    case 8: printf("Invalid choice");
                    break;
 			 
-	}
+}
+     test_main(); 
       return 0;
 }
