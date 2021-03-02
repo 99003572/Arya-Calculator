1f1de046f1e1623f2a78ae454ca423b4fa34362e
99003571 <78537779+Mohammad-Fayaz3571@users.noreply.github.com>
2021-03-02 11:24:11 +0530

    Update exponential.c

--- 3.Implementation/src/exponential.c
+++ 3.Implementation/src/exponential.c
@@ -1,7 +1,7 @@
 #include "header.h"
-int exponential(int num1, int num2)
+float exponential(float num1, float num2)
 {
-    long long result = 1;
+    double result = 1;
     while (num2 != 0) {
         result *= num1;
         --num2;
