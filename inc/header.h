3eed3c09e2c3ea10375c43314bc2655fd1b4dd78
99003514 <69049989+stepin104991@users.noreply.github.com>
2021-03-02 11:47:41 +0530

    Update header.h

--- inc/header.h
+++ inc/header.h
@@ -4,12 +4,12 @@
 
 #include <stdio.h>
 #include <math.h>
-double add(int num1, int num2);
-float subtract(int num1, int num2);
-float multiply(int num1, int num2);
-float division(int num1, int num2);
-Long double exponential(int num);
-float factorial(int num);
-float modulus(int num1, int num2);
+double add(float num1, float num2);
+float subtract(float num1, float num2);
+float multiply(float num1, float num2);
+float division(float num1, float num2);
+Long double exponential(float num);
+float factorial(float num);
+float modulus(float num1, float num2);
 
 #endif
