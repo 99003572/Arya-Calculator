605de993a2e9cf8cfdbedc89ccc2cc837177e566
99003571 <78537779+Mohammad-Fayaz3571@users.noreply.github.com>
2021-03-02 16:25:51 +0530

    Update percentage.c

--- 3.Implementation/src/percentage.c
+++ 3.Implementation/src/percentage.c
@@ -1,8 +1,8 @@
 #include "header.h"
 
-float percentage(float num1,float num2)
+int percentage(int num1,int num2)
 {
-    float percent;
+    int percent;
    
     scanf("%f %f", &num1,&num2);
     if(num1==0 || num2==0)
